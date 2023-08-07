291f22d1c75a881a405b82689857e0846ca24ba7
dev <113306065+dev4lov@users.noreply.github.com>
2023-08-07 10:23:38 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxhi2B8yGoAYia9GgeZhUnTv1JNCT8npg",
-  authDomain: "linkdin-clone-441b1.firebaseapp.com",
-  projectId: "linkdin-clone-441b1",
-  storageBucket: "linkdin-clone-441b1.appspot.com",
-  messagingSenderId: "300506042693",
-  appId: "1:300506042693:web:1a9862ff47a9473ef5ee2c",
+  // Firbase Config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
